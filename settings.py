3af12d851fd114c9523abb2073ba55cd0fdf3e3c
githubtohaoyangli <fairzlq@hotmail.com>
2023-09-03 18:41:21 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,149 @@
+import shutil
+import os
+import time
+# del some files
+a=os.path.exists('C:\\liexee')       
+if a==True:
+    os.system('pause')
+    exit(0)
+#with open some  files
+# skip canary dev beta 
+while True:
+    print('Liexe insider settings(OS build : 1.3.2 (watool build) )')
+    print('1.Skip Channel')
+    print('2.Canary Channel')
+    print('3.Dev Channel')
+    print('4.Beta Channel')
+    print('5.Release Preview Channel (RTM)')
+    #00000-00000-00-0-000000-000-000000-0000-0000-0000-00 (skip
+    #0000-000-00000-00000000-00000-0000-0000000000-000000 (canary
+    #000-0000000000-000000-0000-00000-000-0000-00000-0000 (dev
+    #00000-000000000-00000-0-00000000000-0000000-00000000 (beta
+    #000000-000-0000000000-00000000-0000000-000-0-0000-00 ( release preview (RTM) 
+    b = input('PRESS:')
+    if b == '1':
+        print('Applying Changes......')
+        #crate liexee folder
+        os.mkdir('C:\\liexee')
+        os.mkdir('C:\\liexee\\creak')
+        os.mkdir('C:\\liexee\\liexe')
+        os.mkdir('C:\\liexee\\watool')
+        os.mkdir('C:\\liexee\\widgets')
+        #crate creak folder
+        os.mkdir('C:\\liexee\\creak\\liexe')
+        os.mkdir('C:\\liexee\\creak\\liexe\\00000-00000-00-0-000000-000-000000-0000-0000-0000-00')
+        os.mkdir('C:\\liexee\\creak\\liexe\\00000-00000-00-0-000000-000-000000-0000-0000-0000-00\\accounts')    
+        os.mkdir('C:\\liexee\\creak\\liexe\\00000-00000-00-0-000000-000-000000-0000-0000-0000-00\\yes')
+        
+        os.mkdir('C:\\liexee\\creak\\liexe\\00000-00000-00-0-000000-000-000000-0000-0000-0000-00\\yes\\widgets')
+        #crate watool folder
+        os.mkdir('C:\\liexee\\watool\\build')
+        os.mkdir('C:\\liexee\\watool\\channel')
+        os.mkdir('C:\\liexee\\watool\\disabled ID')
+        os.mkdir('C:\\liexee\\watool\\enabled ID')
+        #crate widgets folder
+        os.mkdir('C:\\liexee\\widgets\\SOUR')
+        time.sleep(0.5)
+        os.system('pause')
+        exit(0)
+    elif b == '2':
+        print('Applying Changes......')
+        #crate liexee folder
+        os.mkdir('C:\\liexee')
+        os.mkdir('C:\\liexee\\creak')
+        os.mkdir('C:\\liexee\\liexe')
+        os.mkdir('C:\\liexee\\watool')
+        os.mkdir('C:\\liexee\\widgets')
+        #crate creak folder
+        os.mkdir('C:\\liexee\\creak\\liexe')
+        os.mkdir('C:\\liexee\\creak\\liexe\\0000-000-00000-00000000-00000-0000-0000000000-000000')
+        os.mkdir('C:\\liexee\\creak\\liexe\\0000-000-00000-00000000-00000-0000-0000000000-000000\\accounts')    
+        os.mkdir('C:\\liexee\\creak\\liexe\\0000-000-00000-00000000-00000-0000-0000000000-000000\\yes')
+        
+        os.mkdir('C:\\liexee\\creak\\liexe\\0000-000-00000-00000000-00000-0000-0000000000-000000\\yes\\widgets')
+        #crate watool folder
+        os.mkdir('C:\\liexee\\watool\\build')
+        os.mkdir('C:\\liexee\\watool\\channel')
+        os.mkdir('C:\\liexee\\watool\\disabled ID')
+        os.mkdir('C:\\liexee\\watool\\enabled ID')
+        #crate widgets folder
+        os.mkdir('C:\\liexee\\widgets\\SOUR')
+        time.sleep(0.5)
+        os.system('pause')
+        exit(0)
+    elif b == '3':
+        print('Applying Changes......')
+        #crate liexee folder
+        os.mkdir('C:\\liexee')
+        os.mkdir('C:\\liexee\\creak')
+        os.mkdir('C:\\liexee\\liexeee')
+        os.mkdir('C:\\liexee\\watool')
+        os.mkdir('C:\\liexee\\widgets')
+        #crate creak folder
+        os.mkdir('C:\\liexee\\creak\\liexe')
+        os.mkdir('C:\\liexee\\creak\\liexe\\000-0000000000-000000-0000-00000-000-0000-00000-0000')
+        os.mkdir('C:\\liexee\\creak\\liexe\\000-0000000000-000000-0000-00000-000-0000-00000-0000\\accounts')    
+        os.mkdir('C:\\liexee\\creak\\liexe\\000-0000000000-000000-0000-00000-000-0000-00000-0000\\yes')
+        
+        os.mkdir('C:\\liexee\\creak\\liexe\\000-0000000000-000000-0000-00000-000-0000-00000-0000\\yes\\widgets')
+        #crate watool folder
+        os.mkdir('C:\\liexee\\watool\\build')
+        os.mkdir('C:\\liexee\\watool\\channel')
+        os.mkdir('C:\\liexee\\watool\\disabled ID')
+        os.mkdir('C:\\liexee\\watool\\enabled ID')
+        #crate widgets folder
+        os.mkdir('C:\\liexee\\widgets\\SOUR')
+        time.sleep(0.5)
+        os.system('pause')
+        exit(0)
+    elif b == '4':
+        print('Applying Changes......')
+        #crate liexee folder
+        os.mkdir('C:\\liexee')
+        os.mkdir('C:\\liexee\\creak')
+        os.mkdir('C:\\liexee\\liexe')
+        os.mkdir('C:\\liexee\\watool')
+        os.mkdir('C:\\liexee\\widgets')
+        #crate creak folder
+        os.mkdir('C:\\liexee\\creak\\liexe')
+        os.mkdir('C:\\liexee\\creak\\liexe\\00000-000000000-00000-0-00000000000-0000000-00000000')
+        os.mkdir('C:\\liexee\\creak\\liexe\\00000-000000000-00000-0-00000000000-0000000-00000000\\accounts')    
+        os.mkdir('C:\\liexee\\creak\\liexe\\00000-000000000-00000-0-00000000000-0000000-00000000\\yes')
+        os.mkdir('C:\\liexee\\creak\\liexe\\00000-000000000-00000-0-00000000000-0000000-00000000\\yes\\widgets')
+        #crate watool folder
+        os.mkdir('C:\\liexee\\watool\\build')
+        os.mkdir('C:\\liexee\\watool\\channel')
+        os.mkdir('C:\\liexee\\watool\\disabled ID')
+        os.mkdir('C:\\liexee\\watool\\enabled ID')
+        #crate widgets folder
+        os.mkdir('C:\\liexee\\widgets')
+        os.mkdir('C:\\liexee\\widgets\\SOUR')
+        time.sleep(0.5)
+        os.system('pause')
+        exit(0)
+    elif b == '5':
+        print('Applying Changes......')
+        #crate liexee folder
+        os.mkdir('C:\\liexee')
+        os.mkdir('C:\\liexee\\creak')
+        os.mkdir('C:\\liexee\\liexe')
+        os.mkdir('C:\\liexee\\watool')
+        os.mkdir('C:\\liexee\\widgets')
+        #crate creak folder
+        os.mkdir('C:\\liexee\\creak\\liexe')
+        os.mkdir('C:\\liexee\\creak\\liexe\\000000-000-0000000000-00000000-0000000-000-0-0000-00')
+        os.mkdir('C:\\liexee\\creak\\liexe\\000000-000-0000000000-00000000-0000000-000-0-0000-00\\accounts')    
+        os.mkdir('C:\\liexee\\creak\\liexe\\000000-000-0000000000-00000000-0000000-000-0-0000-00\\yes')
+        
+        os.mkdir('C:\\liexee\\creak\\liexe\\000000-000-0000000000-00000000-0000000-000-0-0000-00\\yes\\widgets')
+        os.mkdir('C:\\liexee\\watool\\build')
+        os.mkdir('C:\\liexee\\watool\\channel')
+        os.mkdir('C:\\liexee\\watool\\disabled ID')
+        os.mkdir('C:\\liexee\\watool\\enabled ID')
+        #crate widgets folder
+        os.mkdir('C:\\liexee\\widgets')
+        os.mkdir('C:\\liexee\\widgets\\SOUR')
+        time.sleep(0.5)
+        os.system('pause')
+        exit(0)
+                   
